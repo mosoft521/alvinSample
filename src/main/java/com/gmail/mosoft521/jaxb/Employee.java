995570e7594f5bc91714c10a065360eac3d38236
Alvin Zhang <mosoft521@gmail.com>
2016-04-30 14:54:56 +0800

    JAXB测试

---
+++ src/main/java/com/gmail/mosoft521/jaxb/Employee.java
@@ -0,0 +1,55 @@
+package com.gmail.mosoft521.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+@XmlType(name = "Employee", propOrder = {"name", "age", "role", "gender"})
+public class Employee {
+    private String name;
+    private String gender;
+    private int age;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee:: Name=" + this.name + " Age=" + this.age + " Gender="
+                + this.gender + " Role=" + this.role;
+    }
+
+}
---
+++ src/test/java/com/gmail/mosoft521/jaxb/TestJAXB.java
@@ -0,0 +1,81 @@
+package com.gmail.mosoft521.jaxb;
+
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/**
+ * Created by Alvin on 2016/4/30 0030.
+ */
+public class TestJAXB {
+    @Test
+    public void testXml2Obj() throws Exception {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("employee.xml");
+        byte[] bytes = new byte[is.available()];
+        is.read(bytes);
+        String xmlStr = new String(bytes);
+        JAXBContext context = JAXBContext.newInstance(Employee.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Employee emp = (Employee) unmarshaller.unmarshal(new StringReader(xmlStr));
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testObj2Xml() {
+        Employee emp = new Employee();
+        emp.setAge(10);
+        emp.setGender("Male");
+        emp.setName("Jane");
+//        emp.setRole("Teacher");
+        emp.setRole("老师");
+//        String xmlStr = TestJAXB.convertToXml(emp, "utf-8");
+        String xmlStr = TestJAXB.convertToXml(emp, "GBK");
+        System.out.println(xmlStr);
+    }
+
+    public static String convertToXml(Object obj, String encoding) {
+        String result = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(obj.getClass());
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, encoding);
+            //
+            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);//作为XML片段
+
+            StringWriter writer = new StringWriter();
+            marshaller.marshal(obj, writer);
+            result = writer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+//        return result.replace("standalone=\"yes\"", ""); //删除standalone="yes"
+    }
+}
+/*
+>>testXml2Obj
+Employee:: Name=Pankaj Age=29 Gender=Male Role=Java Developer
+>>testObj2Xml
+<?xml version="1.0" encoding="GBK" standalone="yes"?>
+<employee>
+    <name>Jane</name>
+    <age>10</age>
+    <role>老师</role>
+    <gender>Male</gender>
+</employee>
+>>
+<?xml version="1.0" encoding="GBK" ?>
+<employee>
+    <name>Jane</name>
+    <age>10</age>
+    <role>老师</role>
+    <gender>Male</gender>
+</employee>
+ */
