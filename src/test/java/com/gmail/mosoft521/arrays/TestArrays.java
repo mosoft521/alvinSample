b3ddbec469b5df1d2821710e9742fcbf1ddb7896
Alvin Zhang <mosoft521@gmail.com>
2016-05-05 11:20:48 +0800

    Arrays.toString()测试

---
+++ src/test/java/com/gmail/mosoft521/arrays/TestArrays.java
@@ -0,0 +1,20 @@
+package com.gmail.mosoft521.arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Alvin on 2016/5/5 0005.
+ */
+public class TestArrays {
+    public static void main(String[] args) {
+        String[] a = {"111"};
+        String[] b = {"aaa", "bbb"};
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+    }
+}
+/*
+[111]
+[aaa, bbb]
+ */
