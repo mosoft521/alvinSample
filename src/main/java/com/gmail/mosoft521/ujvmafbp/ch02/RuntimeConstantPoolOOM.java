a50bb17353203a0e439a7f620fcb962ea13d56a0
Alvin Zhang <mosoft521@gmail.com>
2021-03-16 21:50:48 +0800

    ujvmafbp-ch02

---
+++ src/main/java/com/gmail/mosoft521/ujvmafbp/ch02/RuntimeConstantPoolOOM.java
@@ -0,0 +1,14 @@
+package com.gmail.mosoft521.ujvmafbp.ch02;
+
+public class RuntimeConstantPoolOOM {
+    public static void main(String[] args) {
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);//sun.misc.Version：private static final java.lang.String launcher_name = "java"; [Loaded sun.misc.Version from D:\tools\Java\jdk1.8.0_281\jre\lib\rt.jar]
+    }
+}
+/*
+true
+false
+ */
