8af72f10154528f16e8cf9d36219601808c77497
Alvin Zhang <mosoft521@gmail.com>
2022-04-29 23:10:42 +0800

    ArrayTest&MaxSubArray

---
+++ src/main/java/com/gmail/mosoft521/array/ArrayTest.java
@@ -0,0 +1,11 @@
+package com.gmail.mosoft521.array;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[][] aa = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        System.out.println(aa.length);
+        System.out.println(aa[0].length);
+    }
+}
+//3
+//4
---
+++ src/main/java/com/gmail/mosoft521/array/MaxSubArray.java
@@ -0,0 +1,31 @@
+package com.gmail.mosoft521.array;
+
+public class MaxSubArray {
+    private static int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        // 定义：dp[i] 记录以 nums[i] 为结尾的「最大子数组和」
+        int[] dp = new int[n];
+        // base case
+        // 第一个元素前面没有子数组
+        dp[0] = nums[0];
+        // 状态转移方程
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+        }
+        // 得到 nums 的最大子数组
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+            if (res == dp[i]) {
+                System.out.println("i=" + i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(arr));//6
+    }
+}
